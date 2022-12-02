c6a343aa033a0490e40b5af145cabfd2814f1f3b
Kristex95 <kristex95@gmail.com>
2022-12-02 22:06:00 +0200

    Dev version ready

--- Assets/Scripts/Main_Menu_Script.cs
+++ Assets/Scripts/Main_Menu_Script.cs
@@ -9,18 +9,34 @@ public class Main_Menu_Script : MonoBehaviour
     [SerializeField]
     private GameObject settingsPanel;
     [SerializeField]
-    private AudioSource musicAudio;
+    private GameObject musicAudioPrefab;
     [SerializeField]
     private Scrollbar musicVolumeScrollbar;
     [SerializeField]
     private Scrollbar effectsVolumeScrollbar;
 
+    private AudioSource musicAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         if (!PlayerPrefs.HasKey("effects")) PlayerPrefs.SetFloat("effects", 1f);
+        if (!PlayerPrefs.HasKey("music")) PlayerPrefs.SetFloat("music", 1f);
+
+        if (GameObject.FindGameObjectWithTag("GameMusic") == null)
+        {
+            GameObject musicObj = Instantiate(musicAudioPrefab);
+            musicAudioSource = musicObj.GetComponent<AudioSource>();
+            DontDestroyOnLoad(musicObj);
+        }
+        else
+        {
+            musicAudioSource = GameObject.FindGameObjectWithTag("GameMusic").GetComponent<AudioSource>();
+            musicAudioSource.volume = PlayerPrefs.GetFloat("music");
+        }
+
         effectsVolumeScrollbar.value = PlayerPrefs.GetFloat("effects");
-        DontDestroyOnLoad(musicAudio);
+        musicVolumeScrollbar.value = PlayerPrefs.GetFloat("music");
     }
 
     // Update is called once per frame
@@ -46,11 +62,19 @@ public class Main_Menu_Script : MonoBehaviour
 
     public void SetMusicVolume()
     {
-        musicAudio.volume = musicVolumeScrollbar.value;
+        if (PlayerPrefs.HasKey("music"))
+        {
+            PlayerPrefs.SetFloat("music", musicVolumeScrollbar.value);
+            musicAudioSource.volume = musicVolumeScrollbar.value;
+        }
+
     }
 
     public void SetEffectsVolume()
     {
-        PlayerPrefs.SetFloat("effects", effectsVolumeScrollbar.value);
+        if (PlayerPrefs.HasKey("effects"))
+        {
+            PlayerPrefs.SetFloat("effects", effectsVolumeScrollbar.value);
+        }
     }
 }
